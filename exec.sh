834a948e355089833351a217687a46169683f358
papillon <m.aalif0314@gmail.com>
2025-09-10 05:48:14 +0800

    Modified exec.sh

--- exec.sh
+++ exec.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 docker run -it --gpus all --shm-size 64G \
-    -v /mnt/e/FYP/Main/Pet:/workspace/ \
+    -v /mnt/e/FYP/Main/PetFace:/workspace/ \
     pytorch/pytorch:1.12.0-cuda11.3-cudnn8-runtime bash
 
